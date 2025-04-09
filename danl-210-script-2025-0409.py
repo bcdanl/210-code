b605733b3461f35917a2364f009b1a41083d3ee3
bcdanl <bchoe@geneseo.edu>
2025-04-09 12:11:01 -0400

    code

--- danl-210-script-2025-0409.py
+++ danl-210-script-2025-0409.py
@@ -14,13 +14,17 @@ Created on Wed Apr  9 11:07:55 2025
 # =============================================================================
 import pandas as pd
 
+# this causes error
+# HTTPError: Too Many Requests
 df = pd.read_html('https://finance.yahoo.com/quote/MSFT/history/')
 
 
 
 
-
-
+# %%
+# =============================================================================
+# Using Selenium .get_attribute('outerHTML')
+# =============================================================================
 import pandas as pd
 import os
 import time
@@ -34,6 +38,8 @@ from io import StringIO
 # Create an instance of Chrome options
 options = Options()
 options.add_argument("window-size=1400,1200")  # Set the browser window size to 1400x1200
+options.add_argument('--disable-blink-features=AutomationControlled')  # Prevent detection of automation by disabling blink features
+# options.page_load_strategy = 'eager'  # Load only essential content first, skipping non-critical resources
 
 # Initialize the Chrome WebDriver with the specified options
 driver = webdriver.Chrome(options=options)  # Correct implementation
@@ -42,18 +48,22 @@ driver = webdriver.Chrome(options=options)  # Correct implementation
 # url = 'https://finance.yahoo.com/quote/MSFT/history/'
 # driver.get(url)
 
+
+# %%
 # Load content page
 url = 'https://finance.yahoo.com/quote/MSFT/history/?p=MSFT&period1=1672531200&period2=1743379200'
 driver.get(url)
-time.sleep(random.uniform(3, 5))  # wait for table to load
+time.sleep(random.uniform(2, 3))  # wait for table to load
 
 
 # Extract the <table> HTML element
 table_html = driver.find_element(By.TAG_NAME, 'table').get_attribute('outerHTML')
 
 # Parse the HTML table into a pandas DataFrame
+# df = pd.read_html(StringIO(table_html))[0]
+
 df = pd.read_html(table_html)[0]
-df[0]
+
 
 
 # %%
@@ -65,7 +75,7 @@ df_all = pd.DataFrame()
 for symbol in symbols:
     url = f'https://finance.yahoo.com/quote/{symbol}/history/?p={symbol}&period1=1672531200&period2=1743379200'
     driver.get(url)
-    time.sleep(random.uniform(3, 5))  # wait for table to load
+    time.sleep(random.uniform(2, 3))  # wait for table to load
     table_html = driver.find_element(By.TAG_NAME, 'table').get_attribute('outerHTML')
     df = pd.read_html(table_html)[0]
     df_all = pd.concat([df_all, df], ignore_index=True)
